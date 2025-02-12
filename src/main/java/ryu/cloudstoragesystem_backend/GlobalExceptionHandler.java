3fe71a3de1e6211f593033c5bb709f8baa0854ad
KizunaAikawa <ryuseikaku9809@gmail.com>
2025-02-12 10:20:17 +0900

    使用更灵活的方式返回错误时响应体的message

--- src/main/java/ryu/cloudstoragesystem_backend/GlobalExceptionHandler.java
+++ src/main/java/ryu/cloudstoragesystem_backend/GlobalExceptionHandler.java
@@ -36,35 +36,35 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponseBody> handleExpiredJwtException(ExpiredJwtException exception) {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponseBody("401", "Token expired"));
+                .body(new ErrorResponseBody("401", exception.getMessage()));
     }
 
     @ExceptionHandler(TokenUnavailableException.class)
     public ResponseEntity<ErrorResponseBody> handleTokenUnavailableException(TokenUnavailableException exception) {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponseBody("401", "Token unavailable"));
+                .body(new ErrorResponseBody("401", exception.getMessage()));
     }
 
     @ExceptionHandler(UsernameConflictException.class)
     public ResponseEntity<ErrorResponseBody> handleUsernameConflictException(UsernameConflictException exception) {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
-                .body(new ErrorResponseBody("409", "Username already exists"));
+                .body(new ErrorResponseBody("409", exception.getMessage()));
     }
 
     @ExceptionHandler(LoginFailException.class)
     public ResponseEntity<ErrorResponseBody> handleLoginFailException(LoginFailException exception) {
         return ResponseEntity
                 .status(HttpStatus.FORBIDDEN)
-                .body(new ErrorResponseBody("403", "Login fail"));
+                .body(new ErrorResponseBody("403", exception.getMessage()));
     }
 
     @ExceptionHandler(UserNotExistException.class)
     public ResponseEntity<ErrorResponseBody> handleUserNotExistException(UserNotExistException exception) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponseBody("404", "User not found"));
+                .body(new ErrorResponseBody("404", exception.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
@@ -72,6 +72,6 @@ public class GlobalExceptionHandler {
         log.error(exception.getMessage(), exception);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponseBody("500", "Unknown Error"));
+                .body(new ErrorResponseBody("500", exception.getMessage()));
     }
 }
